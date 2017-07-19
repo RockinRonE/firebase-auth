c7ad8677cfa8b9e59ca8c29c9b371d94902057f0
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-07-19 11:04:46 -0500

    added handlesubmit fxn

--- components/SignUpForm.js
+++ components/SignUpForm.js
@@ -1,19 +1,30 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { FormLabel, FormInput, Button } from 'react-native-elements'; 
+import axios from 'axios'; 
 
 class SignUpForm extends Component {
 
 	state = { phone : '' };
+
+	handleSubmit = () => {
+		
+	}
 
 	render() {
 		return(
 			<View>
 					<View style={{ marginBottom: 10 }} >
 						<FormLabel>Enter Phone Number</FormLabel>
-						<FormInput />
+						<FormInput 
+							value={this.state.phone} 
+							onChangeText={phone => this.setState({ phone })}
+						/>
 					</View>
-				<Button title="Submit" />
+				<Button 
+					title="Submit" 
+					onPress={this.handleSubmit}
+				/>
 			</View>
 		);
 	}
