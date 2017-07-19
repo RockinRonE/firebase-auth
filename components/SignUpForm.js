a93a7569f4fe07b1351f3bb06c7b78a79a198983
Ronny Ewanek <ronny.ewanek@gmail.com>
2017-07-19 10:50:28 -0500

    initial commit

---
+++ components/SignUpForm.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import { FormLabel, FormInput, Button } from 'react-native-elements'; 
+
+class SignUpForm extends Component {
+
+	state = { phone : '' };
+
+	render() {
+		return(
+			<View>
+					<View style={{ marginBottom: 10 }} >
+						<FormLabel>Enter Phone Number</FormLabel>
+						<FormInput />
+					</View>
+				<Button title="Submit" />
+			</View>
+		);
+	}
+}
+
+export default SignUpForm; 
